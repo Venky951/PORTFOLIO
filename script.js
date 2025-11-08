205ac6effe2ddbbd695a18202042efa2af34fc81
Venky951 <malothvenky12@gmail.com>
2025-11-08 18:27:58 +0530

    changed

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-links");
+function opentab(tabname) {
+        for (tablink of tablinks) {
+                tablink.classList.remove("active-links");
+        }
+        for (tabcontent of tabcontents) {
+                tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-links");
+        document.getElementById(tabname).classList.add("active-tab");
 }
-for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+        sidemenu.style.right = "0";
 }
-event.currentTarget.classList.add("active-links");
-document.getElementById(tabname).classList.add("active-tab");
+function closemenu() {
+        sidemenu.style.right = "-200px";
 }
